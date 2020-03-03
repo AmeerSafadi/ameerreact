1938bf34e494b70235979dd2f12a5e97e7ac3fef
AmeerSafadi <ameersafadi1@gmail.com>
2020-03-03 21:04:54 +0200

    Updates

---
+++ precache-manifest.dc8f9cbf4f29255cd0b90e58484588d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcecf121b15e16b929142bd00adf30b6",
+    "url": "/ameerreact/index.html"
+  },
+  {
+    "revision": "b3106256f58d109dda33",
+    "url": "/ameerreact/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "675647df7470d67c1f6d",
+    "url": "/ameerreact/static/js/2.338e66b7.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/ameerreact/static/js/2.338e66b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3106256f58d109dda33",
+    "url": "/ameerreact/static/js/main.df7d768d.chunk.js"
+  },
+  {
+    "revision": "589e935b484d8c13e369",
+    "url": "/ameerreact/static/js/runtime-main.2ed0ddd7.js"
+  }
+]);
